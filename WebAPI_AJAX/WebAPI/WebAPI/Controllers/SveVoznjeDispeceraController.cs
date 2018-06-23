bfd20b8926b83a550fb1c8c7e74aa2419657566a
Aleksandar Jancikin <apialeksandar@yahoo.com>
2018-06-23 21:05:54 +0200

    ispis voznji u musteriji, dispeceru i vozacu

---
+++ WebAPI_AJAX/WebAPI/WebAPI/Controllers/SveVoznjeDispeceraController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class SveVoznjeDispeceraController : ApiController
+    {
+        public List<Voznja> Get()
+        {
+            List<Voznja> ret = new List<Voznja>();
+
+            foreach (Voznja voznja in Voznje.SveVoznje)
+            {
+                if (voznja.Dispecer.Equals(UlogovaniKorisnici.Dispecer.KorisnickoIme))
+                {
+                    ret.Add(voznja);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ WebAPI_AJAX/WebAPI/WebAPI/Controllers/SveVoznjeVozacaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class SveVoznjeVozacaController : ApiController
+    {
+        public List<Voznja> Get()
+        {
+            List<Voznja> ret = new List<Voznja>();
+
+            foreach (Voznja voznja in Voznje.SveVoznje)
+            {
+                if (voznja.Vozac.Equals(UlogovaniKorisnici.Vozac.KorisnickoIme))
+                {
+                    ret.Add(voznja);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
