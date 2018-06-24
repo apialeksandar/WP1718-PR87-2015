bebde57124533776d9014a2eea2acc5f4ed59f9d
Aleksandar Jancikin <apialeksandar@yahoo.com>
2018-06-24 20:50:08 +0200

    pretraga po ceni musterije

---
+++ WebAPI_AJAX/WebAPI/WebAPI/Controllers/PretragaCenaMusterijaController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+using WebAPI.Models.Temp;
+
+namespace WebAPI.Controllers
+{
+    public class PretragaCenaMusterijaController : ApiController
+    {
+        public List<Voznja> Post(FormirajVoznju temp)
+        {
+            string pom = "";
+            List<Voznja> ret = new List<Voznja>();
+
+            if (!String.IsNullOrEmpty(temp.OdOcena))
+            {
+                if (!String.IsNullOrEmpty(temp.DoOcena))
+                {
+                    pom = "od-do";
+                }
+                else
+                    pom = "od";
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(temp.DoOcena))
+                {
+                    pom = "do";
+                }
+                else
+                {
+                    pom = "od-do";
+                }
+            }
+
+            if (pom.Equals("od"))
+            {
+                foreach (Voznja voznja in Voznje.SveVoznje)
+                {
+                    if ((int)voznja.Iznos >= int.Parse(temp.OdOcena))
+                        ret.Add(voznja);
+                }
+            }
+            else if (pom.Equals("do"))
+            {
+                foreach (Voznja voznja in Voznje.SveVoznje)
+                {
+                    if ((int)voznja.Iznos <= int.Parse(temp.DoOcena))
+                        ret.Add(voznja);
+                }
+            }
+            else if (pom.Equals("od-do"))
+            {
+                foreach (Voznja voznja in Voznje.SveVoznje)
+                {
+                    if ((int)voznja.Iznos >= int.Parse(temp.OdOcena) && (int)voznja.Iznos <= int.Parse(temp.DoOcena))
+                        ret.Add(voznja);
+                }
+            }
+
+            return ret;
+        }
+    }
+}
